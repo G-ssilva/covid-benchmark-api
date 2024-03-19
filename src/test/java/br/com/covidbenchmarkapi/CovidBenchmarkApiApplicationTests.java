ba4d0900f5ed1fbd995662e5762ac186403d696e
Guilherme Silva <guissilva_@outlook.com>
2024-03-19 19:53:26 -0300

    commit inicial

---
+++ src/test/java/br/com/covidbenchmarkapi/CovidBenchmarkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.covidbenchmarkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidBenchmarkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
