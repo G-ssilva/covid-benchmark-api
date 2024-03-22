41db7ea0618fed498a4adccf7e032b697a647bf5
Guilherme Silva <guissilva_@outlook.com>
2024-03-22 10:12:54 -0300

    FEATURE - entityNotFoundException retorna HttpStatus NOT FOUND * Criado novo método no handler de exception

--- src/main/java/br/com/covidbenchmarkapi/exception/ControllerExceptionHandler.java
+++ src/main/java/br/com/covidbenchmarkapi/exception/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.covidbenchmarkapi.exception;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -25,5 +26,11 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     private RestErrorDto unexpectedExceptionHandler(Exception e) {
         return new RestErrorDto(HttpStatus.INTERNAL_SERVER_ERROR, "Ops! Ocorreu um erro inesperado");
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    private RestErrorDto entityNotFoundException(RuntimeException e) {
+        return new RestErrorDto(HttpStatus.NOT_FOUND, e.getMessage());
     }
 }
